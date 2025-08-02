cb8a3e718a03d31d5648bbc4e0b9fa84beb367f6
Waleed <lido.murad@gmail.com>
2025-08-02 21:34:32 +0400

    Add .gitignore and clean up tracked build/deps

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-export declare function checkMultipleImagesStretch(page: any, selectors: string[], threshold?: number): Promise<{
-    selector: string;
-    displayedAspectRatio?: number;
-    naturalAspectRatio?: number;
-    isStretched?: boolean;
-    error?: string;
-}[]>;
